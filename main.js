4b7a7d6517f88651323f390bafc40a3f330ff876
Ilya_Gerasimchik <ilyagerasimchik@gmail.com>
2019-08-30 21:54:53 +0300

    main

---
+++ main.js
@@ -0,0 +1,23 @@
+class TodoApp {
+    dispose(){
+
+    }
+
+    execute() {
+
+    }
+}
+
+class TaskManager {
+    creatTask(title) {
+
+    }
+
+    removeTask(id){
+
+    }
+
+    updateTask() {
+        
+    }
+}
