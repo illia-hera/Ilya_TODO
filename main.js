74f8fca9711f0bce2eee6cf06f64f6eabc5bc31d
Ilya_Gerasimchik <ilyagerasimchik@gmail.com>
2019-08-31 09:56:20 +0300

    discribed classis

--- main.js
+++ main.js
@@ -1,23 +1,55 @@
 class TodoApp {
-    dispose(){
+  dispose(){
 
-    }
+  }
 
-    execute() {
+  execute() {
 
-    }
+  }
 }
 
 class TaskManager {
-    creatTask(title) {
+  constructor(task, render, stor){
+      this.render = render;
+      this.stor = store;
+      this.task = task;
+  }
+  creatTask() {
+    this.task = new Task();
+  }
 
-    }
+  removeTask(id){
 
-    removeTask(id){
+  }
 
-    }
-
-    updateTask() {
-        
-    }
+  updateTask(id) {
+      
+  }
 }
+
+class Task {
+  constructor(title){
+      this._id = this.getRandomId();
+      this._title = title;
+  }
+  getRandomId() {
+      return `'_${Math.random().toString(36).substr(2, 12)}'`;
+  }
+  get id () {
+    return this._id
+  }
+  isDone() {
+      return false;
+  }
+  toggle() {
+
+  }
+}
+
+class Store {
+
+}
+
+const toDo = new Task();
+
+console.log(toDo.id);
